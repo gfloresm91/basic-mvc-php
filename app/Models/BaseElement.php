8444646e21403dbe9ff15ec0e5b33c91d31adf12
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2019-01-17 23:09:00 -0300

    Se agrega concepto de interfaces

--- app/Models/BaseElement.php
+++ app/Models/BaseElement.php
@@ -1,6 +1,8 @@
 <?php
 
-class BaseElement
+require_once 'Printable.php';
+
+class BaseElement implements Printable
 {
     private $title;
     public $description;
@@ -33,5 +35,10 @@ class BaseElement
         $extraMonths = $this->months % 12;
 
         return "$years years $extraMonths months";
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
     }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'BaseElement.php';
+require_once 'Printable.php';
 
 class Job extends BaseElement
 {
