b59d72ab641c296ac0b336917fbc262715b01f36
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2019-01-21 20:35:28 -0300

    Se agrega soft deletes

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\traits\HasDefaultImage;
 
 class Job extends Model
 {
     use HasDefaultImage;
+    use SoftDeletes;
+
     protected $table = 'jobs';
 
     public function getDurationAsString()
