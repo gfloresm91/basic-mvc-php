04da592f2e1dd565135fb93a913ec924dcc31a84
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2019-01-23 15:17:43 -0300

    Se agrega ejemplo de migraciones

---
+++ db/migrations/20190123173942_create_jobs_table.php
@@ -0,0 +1,48 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateJobsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+
+    // Para que esto funcione debes habilitar la extensión PDO_Mysql
+    public function change()
+    {
+        $table = $this->table('jobs');
+        $table
+            ->addColumn('title', 'string')
+            ->addColumn('description', 'string')
+            ->addColumn('months', 'integer', ['null' => true])
+            ->addColumn('filename', 'string', ['null' => true])
+            ->addColumn('created_at', 'datetime', ['null' => true])
+            ->addColumn('updated_at', 'datetime', ['null' => true])
+            ->addColumn('deleted_at', 'datetime', ['null' => true])
+            ->create();
+    }
+}
